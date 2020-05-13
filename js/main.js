07215df40fc04df7f06540d0c3a82cfc1855c002
Alesio Clement <54912841+Alesio-Clement@users.noreply.github.com>
2020-05-13 17:05:06 +0300

    new

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@
   });
 
 })(jQuery);
+
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+    
+    $(".zoom").hover(function(){
+		
+		$(this).addClass('transition');
+	}, function(){
+        
+		$(this).removeClass('transition');
+	});
+});
